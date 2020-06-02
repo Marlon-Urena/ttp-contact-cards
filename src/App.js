11f2471005e3566dd8a801b2e18da3ad123e862b
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-06-02 21:47:35 +0100

    Added Predetermined contact cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import ContactCard from "./contactCard"
+import "./App.css";
 
 function App() {
   return (
-    <h1 className="App">
-      Hello World!
-    </h1>
+    <>
+      <ContactCard
+        name="Ross"
+        mobile="7188888888"
+        work="5555555555"
+        email="ross@example.com"
+      />
+    
+    
+      <ContactCard
+        name="Chandler"
+        mobile="11111111111"
+        work="3472222222"
+        email="chandler@example.com"
+      />
+      <ContactCard
+        name="Jill"
+        mobile="3333333333"
+        work="4444444444"
+        email="jill@example.com"
+      />
+    </>
   );
 }
 
